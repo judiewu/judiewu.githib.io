f0f11e5a1b2e4d6451e750ec584190140463b043
Glitch (hello-webpage) <none>
2020-12-14 03:02:33 +0000

    ⏰🏡 Checkpoint ./script.js:25086319/780 ./index.html:25086319/663 ./style.css:25086319/2405

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
+var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-<script>
-function myFunction() {
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
 }
-</script>
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 
 
